4d81811d4f3eb60def2fc4fe9e8e99ab6f375857
Sajjad <shirazy.sajjad@gmail.com>
2023-07-23 11:31:38 +0330

    feat: layout

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <span class="brand">
+      Open Dictionary
+    </span>
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/docs">Docs</NuxtLink>
+      </li>
+    </ul>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer>All rights are not reserved!</footer>
+</template>
+<style lang="scss">
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
